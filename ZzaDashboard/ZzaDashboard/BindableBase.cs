9dae6058a1cd0d5a3cd9f883adae12f8dfdcff01
Brian Noyes <brian.noyes@solliance.net>
2016-08-13 09:49:17 -0700

    Added ValidatableBindableBase and BindableBase

---
+++ ZzaDashboard/ZzaDashboard/BindableBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZzaDesktop
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        protected virtual void SetProperty<T>(ref T member, T val,
+            [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(member, val)) return;
+
+            member = val;
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
---
+++ ZzaDashboard/ZzaDashboard/ValidatableBindableBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZzaDesktop
+{
+    public class ValidatableBindableBase : BindableBase, INotifyDataErrorInfo
+    {
+        private Dictionary<string, List<string>> _errors = new Dictionary<string, List<string>>();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged = delegate { };
+
+        public System.Collections.IEnumerable GetErrors(string propertyName)
+        {
+            if (_errors.ContainsKey(propertyName))
+                return _errors[propertyName];
+            else
+                return null;
+        }
+
+        public bool HasErrors
+        {
+            get { return _errors.Count > 0; }
+        }
+
+        protected override void SetProperty<T>(ref T member, T val, [CallerMemberName] string propertyName = null)
+        {
+            base.SetProperty<T>(ref member, val, propertyName);
+            ValidateProperty(propertyName, val);
+        }
+
+        private void ValidateProperty<T>(string propertyName, T value)
+        {
+            var results = new List<ValidationResult>();
+            ValidationContext context = new ValidationContext(this);
+            context.MemberName = propertyName;
+            Validator.TryValidateProperty(value, context, results);
+
+            if (results.Any())
+            {
+
+                _errors[propertyName] = results.Select(c => c.ErrorMessage).ToList();
+            }
+            else
+            {
+                _errors.Remove(propertyName);
+            }
+            ErrorsChanged(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
